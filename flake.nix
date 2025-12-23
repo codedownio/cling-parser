473eb4dae5f681919a1aaf21042b2facaef93e03
Tom McLaughlin <tom@codedown.io>
2025-12-23 13:15:33 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,139 @@
+{
+  description = "Minimal C++ parser using Cling interpreter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # Nixpkgs moved to argparse 3.x, but we need ~2.9 (from xeus-cling.nix)
+        argparse_2_9 = pkgs.argparse.overrideAttrs (oldAttrs: {
+          version = "2.9";
+          src = pkgs.fetchFromGitHub {
+            owner = "p-ranav";
+            repo = "argparse";
+            rev = "v2.9";
+            sha256 = "sha256-vbf4kePi5gfg9ub4aP1cCK1jtiA65bUS9+5Ghgvxt/E=";
+          };
+        });
+
+        # Nixpkgs moved to xeus 5.2.0, but we need 3.2.0 (from xeus-cling.nix)
+        xeus_3_2_0 = pkgs.xeus.overrideAttrs (oldAttrs: {
+          version = "3.2.0";
+          src = pkgs.fetchFromGitHub {
+            owner = "jupyter-xeus";
+            repo = "xeus";
+            tag = "3.2.0";
+            sha256 = "sha256-D/dJ0SHxTHJw63gHD6FRZS7O2TVZ0voIv2mQASEjLA8=";
+          };
+          buildInputs = oldAttrs.buildInputs ++ pkgs.lib.singleton pkgs.xtl;
+        });
+
+        # Build our minimal parser (unwrapped)
+        minimal-parser-unwrapped = pkgs.clangStdenv.mkDerivation {
+          pname = "minimal-cling-parser";
+          version = "0.1.0";
+
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.cmake ];
+          buildInputs = with pkgs; [
+            cling.unwrapped
+            cppzmq
+            libuuid
+            llvmPackages_18.llvm
+            ncurses
+            openssl
+            pugixml
+            xeus-zmq
+            xtl
+            zeromq
+            zlib
+          ] ++ [
+            argparse_2_9
+            xeus_3_2_0
+          ];
+
+          cmakeBuildType = "Release";
+
+          meta = with pkgs.lib; {
+            description = "Minimal C++ parser using Cling interpreter";
+            license = licenses.bsd3;
+            platforms = platforms.unix;
+            mainProgram = "minimal-parser";
+          };
+        };
+
+        # Wrapped version with proper cling flags
+        minimal-parser = minimal-parser-unwrapped.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+
+          # minimal-parser needs a collection of flags to start up properly, so wrap it by default.
+          # We'll provide the unwrapped version as a passthru
+          flags = pkgs.cling.flags ++ [
+            "-resource-dir"
+            "${pkgs.cling.unwrapped}"
+            "-L"
+            "${pkgs.cling.unwrapped}/lib"
+            "-l"
+            "${pkgs.cling.unwrapped}/lib/cling.so"
+          ];
+
+          fixupPhase = ''
+            runHook preFixup
+
+            wrapProgram $out/bin/minimal-parser --add-flags "$flags"
+
+            runHook postFixup
+          '';
+
+          passthru = (oldAttrs.passthru or { }) // {
+            unwrapped = minimal-parser-unwrapped;
+          };
+        });
+
+      in
+      {
+        packages = {
+          default = minimal-parser;
+          minimal-parser = minimal-parser;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cmake
+            clang
+            cling.unwrapped
+            cppzmq
+            libuuid
+            llvmPackages_18.llvm
+            ncurses
+            openssl
+            pugixml
+            xeus-zmq
+            xtl
+            zeromq
+            zlib
+          ] ++ [
+            argparse_2_9
+            xeus_3_2_0
+          ];
+
+          shellHook = ''
+            echo "Development shell for minimal Cling parser"
+            echo "Build with: nix build"
+            echo "Run with: ./result/bin/minimal-parser"
+          '';
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = minimal-parser;
+          name = "minimal-parser";
+        };
+      });
+}
---
+++ src/main.cpp
@@ -0,0 +1,209 @@
+#include <iostream>
+#include <string>
+
+#include "cling/Interpreter/Interpreter.h"
+#include "cling/Interpreter/Transaction.h"
+
+#include "clang/AST/Decl.h"
+#include "clang/AST/DeclGroup.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/SourceLocation.h"
+#include "llvm/Support/Casting.h"
+
+#include <set>
+
+using interpreter_ptr = std::unique_ptr<cling::Interpreter>;
+
+interpreter_ptr build_interpreter(int argc, char** argv)
+{
+    int interpreter_argc = argc + 1;
+    const char** interpreter_argv = new const char*[interpreter_argc];
+    interpreter_argv[0] = "minimal-parser";
+
+    // Copy all arguments in the new array excepting the process name.
+    for (int i = 1; i < argc; i++)
+    {
+        interpreter_argv[i] = argv[i];
+    }
+
+    std::cout << "GOT HERE 1" << std::endl;
+
+    std::string include_dir = std::string(LLVM_DIR) + std::string("/include");
+    interpreter_argv[interpreter_argc - 1] = include_dir.c_str();
+
+    std::cout << "GOT HERE 2" << std::endl;
+
+    interpreter_ptr interp_ptr = std::make_unique<cling::Interpreter>(interpreter_argc, interpreter_argv, LLVM_DIR);
+
+    std::cout << "GOT HERE 3" << std::endl;
+
+    delete[] interpreter_argv;
+    return interp_ptr;
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "BEGINNING OF MAIN" << std::endl;
+
+    auto interp = build_interpreter(argc, argv);
+
+    // Test code with actual declarations to parse
+    std::string code = R"(
+#include <iostream>
+
+using namespace std;
+
+int x = 42;
+
+void myFunction(int param) {
+    return;
+}
+
+class MyClass {
+public:
+    int member;
+    void method();
+ };
+
+cout << "hello" << endl;
+)";
+
+    // Try basic Cling parsing
+    cling::Transaction* T = nullptr;
+    std::cerr << "About to parse code: " << code << std::endl;
+    cling::Interpreter::CompilationResult result = interp->parse(code, &T);
+    std::cerr << "Parse completed, result: " << result << ", transaction: " << (void*)T << std::endl;
+
+    if (T && result == cling::Interpreter::kSuccess) {
+        std::cerr << "Transaction details:" << std::endl;
+
+        // Get source manager for location information
+        clang::SourceManager& SM = interp->getCI()->getSourceManager();
+
+        // Count declarations and filter for input source only
+        size_t totalDeclCount = 0;
+        size_t inputDeclCount = 0;
+        std::set<clang::Decl*> seenDecls; // Deduplicate declarations
+
+        // Iterate through the declaration groups in the transaction
+        for (auto it = T->decls_begin(); it != T->decls_end(); ++it) {
+            const cling::Transaction::DelayCallInfo& callInfo = *it;
+            clang::DeclGroupRef DGR = callInfo.m_DGR;
+
+            // Iterate through individual declarations in this group
+            for (auto declIt = DGR.begin(); declIt != DGR.end(); ++declIt) {
+                clang::Decl* decl = *declIt;
+                totalDeclCount++;
+
+                if (decl) {
+                    clang::SourceLocation loc = decl->getLocation();
+
+                    // Check if this declaration is from our input (not from system headers)
+                    if (loc.isValid()) {
+                        clang::FileID fileID = SM.getFileID(loc);
+                        const clang::FileEntry* fileEntry = SM.getFileEntryForID(fileID);
+
+                        // Input code shows up as special "input_line_X" files or similar
+                        std::string filename = "";
+                        if (fileEntry) {
+                            filename = std::string(fileEntry->getName());
+                        } else {
+                            // For memory buffers/input, use the buffer name
+                            if (auto bufferPtr = SM.getBufferOrNone(fileID)) {
+                                filename = bufferPtr->getBufferIdentifier().str();
+                            }
+                        }
+
+                        // Filter: ONLY show declarations from actual input lines (much more restrictive)
+                        bool isFromInput = filename.find("input_line_") != std::string::npos;
+
+                        // Skip if we've already seen this declaration (deduplicate)
+                        if (isFromInput && seenDecls.find(decl) == seenDecls.end()) {
+                            seenDecls.insert(decl);
+
+                            inputDeclCount++;
+
+                            // Get line/column information for start and end
+                            clang::SourceRange sourceRange = decl->getSourceRange();
+                            clang::SourceLocation startLoc = sourceRange.getBegin();
+                            clang::SourceLocation endLoc = sourceRange.getEnd();
+
+                            clang::PresumedLoc startPresumedLoc = SM.getPresumedLoc(startLoc);
+                            clang::PresumedLoc endPresumedLoc = SM.getPresumedLoc(endLoc);
+
+                            std::cerr << "  Declaration from input:" << std::endl;
+                            std::cerr << "    Type: " << decl->getDeclKindName() << std::endl;
+                            std::cerr << "    Pointer: " << (void*)decl << std::endl;
+                            std::cerr << "    Start: " << filename;
+                            if (startPresumedLoc.isValid()) {
+                                std::cerr << " " << startPresumedLoc.getLine()
+                                          << ":" << startPresumedLoc.getColumn();
+                            }
+                            std::cerr << std::endl;
+                            std::cerr << "    End: " << filename;
+                            if (endPresumedLoc.isValid()) {
+                                std::cerr << " " << endPresumedLoc.getLine()
+                                          << ":" << endPresumedLoc.getColumn();
+                            }
+                            std::cerr << std::endl;
+
+                            // Try to get more specific information
+                            if (clang::NamedDecl* namedDecl = llvm::dyn_cast<clang::NamedDecl>(decl)) {
+                                std::cerr << "    Name: " << namedDecl->getNameAsString() << std::endl;
+                            }
+
+                            // Show additional info for function declarations
+                            if (clang::FunctionDecl* funcDecl = llvm::dyn_cast<clang::FunctionDecl>(decl)) {
+                                std::cerr << "    Function return type: "
+                                         << funcDecl->getReturnType().getAsString() << std::endl;
+                                std::cerr << "    Number of parameters: "
+                                         << funcDecl->getNumParams() << std::endl;
+                                if (funcDecl->hasBody()) {
+                                    std::cerr << "    Has body: yes" << std::endl;
+                                } else {
+                                    std::cerr << "    Has body: no (declaration only)" << std::endl;
+                                }
+                            }
+
+                            // Show additional info for variable declarations
+                            if (clang::VarDecl* varDecl = llvm::dyn_cast<clang::VarDecl>(decl)) {
+                                std::cerr << "    Variable type: "
+                                         << varDecl->getType().getAsString() << std::endl;
+                            }
+
+                            // Show additional info for class/struct declarations
+                            if (clang::CXXRecordDecl* recordDecl = llvm::dyn_cast<clang::CXXRecordDecl>(decl)) {
+                                std::cerr << "    Record type: " << (recordDecl->isClass() ? "class" : "struct") << std::endl;
+                                if (recordDecl->isCompleteDefinition()) {
+                                    std::cerr << "    Complete definition: yes" << std::endl;
+                                } else {
+                                    std::cerr << "    Complete definition: no (forward declaration)" << std::endl;
+                                }
+                                std::cerr << "    Definition pointer: " << (void*)recordDecl->getDefinition() << std::endl;
+                                std::cerr << "    Is this definition: " << (recordDecl->isThisDeclarationADefinition() ? "yes" : "no") << std::endl;
+                            }
+
+                            // Show additional info for field declarations
+                            if (clang::FieldDecl* fieldDecl = llvm::dyn_cast<clang::FieldDecl>(decl)) {
+                                std::cerr << "    Field type: "
+                                         << fieldDecl->getType().getAsString() << std::endl;
+                            }
+
+                            std::cerr << std::endl;
+                        }
+                    }
+                }
+            }
+        }
+
+        std::cerr << "Summary:" << std::endl;
+        std::cerr << "  Total declarations: " << totalDeclCount << std::endl;
+        std::cerr << "  Declarations from input: " << inputDeclCount << std::endl;
+
+    } else {
+        std::cerr << "No valid transaction or parsing failed (result: " << result << ")" << std::endl;
+    }
+
+    return 0;
+}
