d39ab1457cd05e0e7412517e22b10eca68748a9a
Tom McLaughlin <tom@codedown.io>
2026-01-04 14:03:22 -0800

    Detect preprocessor macros

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <regex>
 
 #include "cling/Interpreter/Interpreter.h"
 #include "cling/Interpreter/Transaction.h"
@@ -9,6 +11,8 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Basic/SourceLocation.h"
+#include "clang/Lex/PPCallbacks.h"
+#include "clang/Lex/Preprocessor.h"
 #include "llvm/Support/Casting.h"
 
 #include <set>
@@ -17,6 +21,104 @@
 #include <fstream>
 
 using interpreter_ptr = std::unique_ptr<cling::Interpreter>;
+
+struct PreprocessorDirective {
+    std::string type;
+    int line;
+    int startCol;
+    int endCol;
+};
+
+class PreprocessorCallbacks : public clang::PPCallbacks {
+private:
+    std::vector<PreprocessorDirective>& directives_;
+    clang::SourceManager& sourceManager_;
+
+public:
+    PreprocessorCallbacks(std::vector<PreprocessorDirective>& directives,
+                         clang::SourceManager& sourceManager)
+        : directives_(directives), sourceManager_(sourceManager) {}
+
+    void InclusionDirective(
+        clang::SourceLocation HashLoc,
+        const clang::Token& IncludeTok,
+        llvm::StringRef FileName,
+        bool IsAngled,
+        clang::CharSourceRange FilenameRange,
+        clang::OptionalFileEntryRef File,
+        llvm::StringRef SearchPath,
+        llvm::StringRef RelativePath,
+        const clang::Module* Imported,
+        clang::SrcMgr::CharacteristicKind FileType) override {
+
+        // Only capture includes from our input source
+        if (HashLoc.isValid()) {
+            clang::FileID fileID = sourceManager_.getFileID(HashLoc);
+            const clang::FileEntry* fileEntry = sourceManager_.getFileEntryForID(fileID);
+
+            std::string filename = "";
+            if (fileEntry) {
+                filename = std::string(fileEntry->getName());
+            } else {
+                // For memory buffers/input, use the buffer name
+                if (auto bufferPtr = sourceManager_.getBufferOrNone(fileID)) {
+                    filename = bufferPtr->getBufferIdentifier().str();
+                }
+            }
+
+            // Filter: ONLY show includes from actual input lines
+            bool isFromInput = filename.find("input_line_") != std::string::npos ||
+                              filename.empty() || filename == "<stdin>";
+
+            if (isFromInput) {
+                clang::PresumedLoc presumedLoc = sourceManager_.getPresumedLoc(HashLoc);
+                if (presumedLoc.isValid()) {
+                    PreprocessorDirective directive;
+                    directive.type = "Include";
+                    directive.line = presumedLoc.getLine();
+                    directive.startCol = presumedLoc.getColumn();
+                    directive.endCol = directive.startCol + 8 + FileName.size(); // Rough estimate
+                    directives_.push_back(directive);
+                }
+            }
+        }
+    }
+
+    void MacroDefined(const clang::Token& MacroNameTok,
+                      const clang::MacroDirective* MD) override {
+        clang::SourceLocation loc = MacroNameTok.getLocation();
+        if (loc.isValid()) {
+            clang::FileID fileID = sourceManager_.getFileID(loc);
+            const clang::FileEntry* fileEntry = sourceManager_.getFileEntryForID(fileID);
+
+            std::string filename = "";
+            if (fileEntry) {
+                filename = std::string(fileEntry->getName());
+            } else {
+                // For memory buffers/input, use the buffer name
+                if (auto bufferPtr = sourceManager_.getBufferOrNone(fileID)) {
+                    filename = bufferPtr->getBufferIdentifier().str();
+                }
+            }
+
+            // Filter: ONLY show defines from actual input lines
+            bool isFromInput = filename.find("input_line_") != std::string::npos ||
+                              filename.empty() || filename == "<stdin>";
+
+            if (isFromInput) {
+                clang::PresumedLoc presumedLoc = sourceManager_.getPresumedLoc(loc);
+                if (presumedLoc.isValid()) {
+                    PreprocessorDirective directive;
+                    directive.type = "Define";
+                    directive.line = presumedLoc.getLine();
+                    directive.startCol = presumedLoc.getColumn();
+                    directive.endCol = directive.startCol + MacroNameTok.getLength();
+                    directives_.push_back(directive);
+                }
+            }
+        }
+    }
+};
 
 interpreter_ptr build_interpreter(int argc, char** argv)
 {
@@ -46,12 +148,36 @@ int main(int argc, char* argv[])
 
     auto interp = build_interpreter(argc, argv);
 
-    // Try basic Cling parsing
+    // Set up preprocessor callbacks to capture directives
+    std::vector<PreprocessorDirective> preprocessorDirectives;
+    clang::SourceManager& SM = interp->getCI()->getSourceManager();
+    clang::Preprocessor& PP = interp->getCI()->getPreprocessor();
+
+    auto callbacks = std::make_unique<PreprocessorCallbacks>(preprocessorDirectives, SM);
+    PP.addPPCallbacks(std::move(callbacks));
+
+    // Try basic Cling parsing for declarations (this will trigger the callbacks)
     cling::Transaction* T = nullptr;
     cling::Interpreter::CompilationResult result = interp->parse(code, &T);
 
     std::cout << "[";
     bool first = true;
+
+    // Output preprocessor directives first
+    for (const auto& directive : preprocessorDirectives) {
+        if (!first) {
+            std::cout << ",";
+        }
+        first = false;
+
+        std::cout << "{";
+        std::cout << "\"type\":\"" << directive.type << "\"";
+        std::cout << ",\"start_line\":" << directive.line;
+        std::cout << ",\"start_ch\":" << directive.startCol;
+        std::cout << ",\"end_line\":" << directive.line;
+        std::cout << ",\"end_ch\":" << directive.endCol;
+        std::cout << "}";
+    }
 
     if (T && result == cling::Interpreter::kSuccess) {
         // Get source manager for location information
